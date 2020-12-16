f40321c842359d111d646f7e8916bd58cc3c73ae
shadabbahadara <shadabbahadara@gmail.com>
2020-12-16 13:34:34 +0530

    removed multithreading programs

--- src/main/java/com/khan/thread/MyThread.java
+++
@@ -1,8 +0,0 @@
-package com.khan.thread;
-
-public class MyThread extends Thread {
-	@Override
-	public void run() {
-		System.out.println("Inside thread.......");
-	}
-}
--- src/main/java/com/khan/threadpool/Task.java
+++
@@ -1,5 +0,0 @@
-package com.khan.threadpool;
-
-public interface Task {
-	void run();
-}
